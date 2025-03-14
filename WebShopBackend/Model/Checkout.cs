3f3222d2a69ba3cd3bcd1ca89cf50674b0b61681
Farah Alhaddad <Farah.alhaddad@hotmail.com>
2025-03-14 18:31:39 +0100

    Add Checkout class to WebShopBackend.Model
    Introduced a new `Checkout` class with properties for
    `ID`, `TimePurchased`, a required `Customer`, and a
    list of `Products`.

---
+++ WebShopBackend/Model/Checkout.cs
@@ -0,0 +1,10 @@
+﻿namespace WebShopBackend.Model
+{
+    public class Checkout
+    {
+        public int ID { get; set; }
+        public DateTime TimePurchased { get; set;}
+        public required Customer Customer { get; set; } = new();
+        public List<Product> Products { get; set; } = new();
+    }
+}
