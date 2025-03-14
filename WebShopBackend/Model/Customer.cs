a6c5a1e59260a908a6ec2e80776f051eab950d10
Farah Alhaddad <Farah.alhaddad@hotmail.com>
2025-03-14 18:31:13 +0100

    Add Customer class to WebShopBackend.Model namespace
    Introduced a new `Customer` class with properties for ID,
    FirstName, LastName, Email, Street, City, ZipCode, and
    collections for Products and Checkouts. String properties
    are initialized to `null!` to enforce non-nullability.

---
+++ WebShopBackend/Model/Customer.cs
@@ -0,0 +1,15 @@
+﻿namespace WebShopBackend.Model
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Street { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public int ZipCode { get; set; }
+        public List<Product> Products { get; set; } = new();
+        public List<Checkout> Checkouts { get; set; } = new();
+    }
+}
