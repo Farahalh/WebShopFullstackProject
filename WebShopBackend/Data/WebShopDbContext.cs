7c4b8e3ce3bd445bf7808f1e6ff6dbaf592c7ab1
Farah Alhaddad <Farah.alhaddad@hotmail.com>
2025-03-14 18:32:04 +0100

    Add WebShopDbContext for entity management
    Introduces the `WebShopDbContext` class inheriting from `DbContext`.
    This class includes `DbSet` properties for `Checkout`, `Customer`,
    and `Product` entities. It also overrides `OnConfiguring` to enable
    database operation logging to the debug console with sensitive data
    logging for improved debugging capabilities.

---
+++ WebShopBackend/Data/WebShopDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using WebShopBackend.Model;
+
+namespace WebShopBackend.Data
+{
+    public class WebShopDbContext : DbContext
+    {
+        public DbSet<Checkout> Checkouts { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+    
+    public WebShopDbContext(DbContextOptions<WebShopDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.LogTo(message => Debug.WriteLine(message))
+                .EnableSensitiveDataLogging();
+        }
+    }
+}
